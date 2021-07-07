c5736421684eab571e9d20f99232a2999e2ebe47
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-07-07 12:31:26 +0200

    js_studies

---
+++ add.js
@@ -0,0 +1,83 @@
+// Loops
+
+//*********************************************************** */
+
+// The following loop statements are given by JavaScript to accomplish this:
+
+// ● while statement
+// ● for statement
+// ● for…in statement
+// ● for…of statement
+// ● do..while statement
+// ● break statement
+// ● continue statement
+
+//*********************************************************** */
+
+// while statement
+
+// while (condition) {
+//     // statement
+// }
+
+//--------------------------
+
+// var i = 0;
+// var sum = 0;
+
+// while (i < 6) {
+//     var sum = sum + i;
+//     i++;
+// }
+// console.log("The total is: " + sum);
+
+//--------------------------
+
+// var i = 0;
+// var mult = 5;
+
+// while (i < 3) {
+//     var mult = mult * i;
+//     i++;
+// }
+// console.log("The answer is:" + mult);
+
+//*********************************************************** */
+
+// do..while statement
+
+// do {
+//     statement
+// } while (condition);
+
+//--------------------------
+
+// var i = 6;
+// var sum = 0;
+
+// do {
+//     var sum = sum + i;
+//     i++;
+// }
+// while (i < 6);
+
+// console.log("the total is " + sum);
+
+//*********************************************************** */
+
+// for statement
+
+// for (statement 1; statement 2; statement 3)
+//   // code block
+
+//--------------------------
+
+// var sum=0;
+// for (var i=0; i<6; i++){
+//     sum+=i;
+// }
+// console.log("the total is " + sum);
+
+//--------------------------
+
+
